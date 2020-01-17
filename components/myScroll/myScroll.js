4938ba99ceafe300920f4e7e05a4c6aa7ae79bd1
chenweiyuan <chenweiyuan@itcast.cn>
2020-01-17 17:27:58 +0800

    首页布局修复

---
+++ components/myScroll/myScroll.js
@@ -0,0 +1,26 @@
+// components/myScroll/myScroll.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    leftList: {
+      type: Array,
+      value: []
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
